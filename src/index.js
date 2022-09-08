db6c3989cd91daef873282d2549814abb91b7eab
Martovytskyi Pavlo <viy.1979@gmail.com>
2022-09-08 11:28:35 +0300

    start

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ const key = '29779995-417a1bba921000e7251cb80ce';
 const image_type = 'photo';
 const orientation = 'horizontal';
 const safesearch = 'true';
+const per_page = 40;
+let page = 1;
+
+
+function fetchImages(name) {
+    
+}
