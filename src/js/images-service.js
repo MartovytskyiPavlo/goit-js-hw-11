5972a776a7c8c63c66a253c8e14345bdb39f7776
u20142 <u20142@access.local>
2022-09-27 12:55:54 +0300

    start

--- src/js/images-service.js
+++ src/js/images-service.js
@@ -1,25 +1,35 @@
+const axios = require('axios').default;
+
 export default class ImagesApiService{
     constructor() { 
         this.searchQuery = '';
         this.page = 1;
         this.enableNextPage = false;
+        this.newSearch = true;
     }
     
-    fetchImages() { 
+    async fetchImages() { 
         if (!this.query) { return; }
 
         const API_URL = "https://pixabay.com/api/"; 
         const key = '29779995-417a1bba921000e7251cb80ce';
 
+
         const URL = `${API_URL}?key=${key}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
 
-        return fetch(URL)
-            .then(r => r.json())
-            .then(data => {
-                this.incrementPage();
-                this.enableNextPage = (data.totalHits > this.page * 40);
-                return data;
-            });
+        const { data } = await axios.get(URL);
+        this.incrementPage();
+        this.enableNextPage = (data.totalHits > this.page * 40);
+
+        return data;
+
+        // return fetch(URL)
+        //     .then(r => r.json())
+        //     .then(data => {
+        //         this.incrementPage();
+        //         this.enableNextPage = (data.totalHits > this.page * 40);
+        //         return data;
+        //     });
     }
 
     incrementPage() {
@@ -28,6 +38,7 @@ export default class ImagesApiService{
 
     resetPage() {
         this.page = 1;
+        this.newSearch = true;
     }
 
 
